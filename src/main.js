d239d9cc8a4ec910a43ab633f0f6e2bdbc911a7e
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-04-07 17:15:24 -0700

    Added Rocket.js

--- src/main.js
+++ src/main.js
@@ -5,4 +5,12 @@ let config = {
     scene: [ Menu, Play ]
 }
 
+// instantiate the game
 let game = new Phaser.Game(config);
+
+// set UI size
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
